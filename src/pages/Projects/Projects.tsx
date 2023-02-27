fab9150150245f76417012dd4c0ab56fbf47cd39
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-27 14:03:47 -0700

    Create Projects.tsx

---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Projects: React.FC = () => {
+	return <Main id={"projects"}></Main>;
+};
+
+const Main = styled(motion.section)`
+	min-height: 100vh;
+	background: var(--palette-purple-2);
+`;
+
+export default Projects;
