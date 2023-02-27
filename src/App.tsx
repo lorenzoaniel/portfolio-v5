48a123a3fb65ad468b18b39c2de381b31f0837be
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-26 19:41:11 -0700

    App: added all necessary sections and working navbar
    site is barebones for now but I have the navbar working on all browsers including mobile. will have to implement some kind of loading screen before moving on

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import styled from "styled-components";
+import React, { Suspense } from "react";
+
 import { GlobalStyle } from "./styles/GlobalStyle";
+
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import NavbarDefault from "./components/Navbar/NavbarDefault";
 
 const App: React.FC = () => {
+	//RENDER
 	return (
 		<>
 			<GlobalStyle />
 			<Main>
-				<Home />
-				<Projects />
-				<Contact />
+				<NavbarDefault />
+				<Suspense fallback={<div>Loading...</div>}>
+					<Home />
+					<Projects />
+					<Contact />
+				</Suspense>
 			</Main>
 		</>
 	);
@@ -19,8 +27,10 @@ const App: React.FC = () => {
 
 const Main = styled.section`
 	height: fit-content;
-	width: inherit;
+	width: 100%;
 	background: purple;
+	display: flex;
+	flex-direction: column;
 `;
 
 export default App;
