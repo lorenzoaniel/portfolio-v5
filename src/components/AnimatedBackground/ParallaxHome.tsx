7b715d4f7824ec3683f57a697949c5110b676ff4
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-27 14:02:36 -0700

    Create ParallaxHome.tsx

---
+++ src/components/AnimatedBackground/ParallaxHome.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const ParallaxHome = () => {
+	return <div>ParallaxHome</div>;
+};
+
+export default ParallaxHome;
