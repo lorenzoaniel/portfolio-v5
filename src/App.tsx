71016765e921724e2d109d202760457d4b1e24c8
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-28 16:29:28 -0700

    Added Parallax
    other half (use Transform) is in Parallax component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
 
@@ -7,13 +7,19 @@ import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
 import NavbarDefault from "./components/Navbar/NavbarDefault";
+import ParallaxHome from "./components/AnimatedBackground/ParallaxHome";
+import { useScroll } from "framer-motion";
 
 const App: React.FC = () => {
+	// const ref = useRef(null);
+	const { scrollYProgress } = useScroll({ offset: ["start end", "end end"] });
+
 	//RENDER
 	return (
 		<>
 			<GlobalStyle />
-			<Main>
+			<Main id={"App"}>
+				<ParallaxHome scrollY={scrollYProgress} />
 				<NavbarDefault />
 				<Suspense fallback={<div>Loading...</div>}>
 					<Home />
@@ -28,9 +34,10 @@ const App: React.FC = () => {
 const Main = styled.section`
 	height: fit-content;
 	width: 100%;
-	background: purple;
+	background: var(--palette-color-light);
 	display: flex;
 	flex-direction: column;
+	z-index: -99;
 `;
 
 export default App;
