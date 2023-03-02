e4f4f5b876565131193b5724053f56655d0d8be5
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-02 16:19:41 -0700

    Finished Animating

--- src/components/Heading/HeroHeading.tsx
+++ src/components/Heading/HeroHeading.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, useAnimationControls, useScroll, useSpring, useTransform } from "framer-motion";
 import { motionPropsDefault } from "../../helpers/misc/motionPropsDefault";
 import { GiStrikingDiamonds } from "react-icons/gi";
 
@@ -9,44 +9,129 @@ interface Props {
 }
 
 const HeroHeading: React.FC<Props> = ({ title }) => {
+	let { scrollY } = useScroll();
+	const opacity = useSpring(useTransform(scrollY, [0, 1000], [1, 0]));
+
+	//TitleMain
+	const titleMainCont = useAnimationControls();
+	const yTitleMain = useSpring(useTransform(scrollY, [0, 1], [0, 0.5], { clamp: false }));
+	let titleMainParallaxStyle = {
+		y: yTitleMain,
+		opacity: opacity,
+	};
+
+	//Icons
+	const IconsOnPageLoadCont = useAnimationControls();
+	const yIcons = useSpring(useTransform(scrollY, [0, 1], [0, 0.25], { clamp: false }));
+	const rotateLeftIcons = useSpring(useTransform(scrollY, [0, 1000], [0, -45]));
+	const rotateRightIcons = useSpring(useTransform(scrollY, [0, 1000], [0, 45]));
+	let IconsParallaxStyle = {
+		Left: {
+			y: yIcons,
+			opacity: opacity,
+			rotate: rotateLeftIcons,
+		},
+		Right: {
+			y: yIcons,
+			opacity: opacity,
+			rotate: rotateRightIcons,
+		},
+	};
+
+	//Place all Seq func here to orchestrate initial pageload
+	const MainLoadSeq = async () => {
+		// no await so it can start same time as icons
+		titleMainCont.start({
+			y: 0,
+			opacity: 1,
+		});
+
+		await IconsOnPageLoadCont.start({
+			y: 0,
+			rotate: 0,
+			opacity: 1,
+		});
+	};
+
+	useEffect(() => {
+		MainLoadSeq();
+	}, []);
+
+	console.log("render hero");
+
 	return (
-		<Main {...motionPropsDefault}>
+		<Main
+			initial={{
+				opacity: 0,
+			}}
+			animate={{
+				opacity: 1,
+				transition: {
+					duration: 0.5,
+					ease: "easeInOut",
+				},
+			}}
+		>
 			<IcongroupMain>
-				<IconLeftMain>
-					<GiStrikingDiamonds style={iconStyle} />
+				<IconLeftMain
+					initial={{
+						opacity: 0,
+						y: 200,
+						rotate: -90,
+					}}
+					onMouseOver={() => {}}
+					animate={IconsOnPageLoadCont}
+					style={IconsParallaxStyle.Left}
+				>
+					<GiStrikingDiamonds style={{ ...iconStyle, transform: "scaleY(-1) rotate(90deg)" }} />
 				</IconLeftMain>
-				<IconRightMain>
-					<GiStrikingDiamonds style={iconStyle} />
+				<IconRightMain
+					initial={{
+						opacity: 0,
+						y: 200,
+						rotate: 90,
+					}}
+					animate={IconsOnPageLoadCont}
+					style={IconsParallaxStyle.Right}
+				>
+					<GiStrikingDiamonds style={{ ...iconStyle, transform: "rotate(-90deg)" }} />
 				</IconRightMain>
 			</IcongroupMain>
 
-			<TitleMain>{title}</TitleMain>
+			<TitleMain
+				initial={{
+					y: -200,
+					opacity: 0,
+				}}
+				animate={titleMainCont}
+				style={titleMainParallaxStyle}
+			>
+				{title}
+			</TitleMain>
 		</Main>
 	);
 };
 
+//HEROMAINCONTAINER
 const Main = styled(motion.h1)`
-	/* background: red; */
-	/* border: 0.1rem solid red; */
-	height: clamp(20rem, 50vw + 0.5rem, 70rem); // 70
+	height: clamp(20rem, 50vw + 0.5rem, 70rem);
 	width: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	overflow-x: hidden;
+	overflow: hidden;
+	/* transition: all 0.3 ease-in-out; */
 `;
 
+//TITLE
 const TitleMain = styled(motion.div)`
-	/* border: 0.1rem solid blue; */
-	font-size: clamp(1.5rem, 5vw + 0.5rem, 6rem); //1.5rem
+	font-size: clamp(1.5rem, 5vw + 0.5rem, 6rem);
 	align-self: flex-start;
 	margin-top: 3%;
 `;
 
 //ICON
 const IcongroupMain = styled(motion.div)`
-	/* border: 0.1rem solid orange; */
-
 	height: fit-content;
 	width: fit-content;
 	position: absolute;
@@ -54,16 +139,11 @@ const IcongroupMain = styled(motion.div)`
 `;
 
 const IconRightMain = styled(motion.div)`
-	/* border: 0.1rem solid lightblue; */
-	height: clamp(13rem, 45vw + 0.5rem, 60rem); // 18,
+	height: clamp(13rem, 45vw + 0.5rem, 60rem);
 	width: clamp(13rem, 45vw + 0.5rem, 60rem);
-	transform: rotate(-90deg);
 `;
 
-const IconLeftMain = styled(IconRightMain)`
-	/* transform: rotate(-100deg); */
-	transform: scaleY(-1) rotate(90deg);
-`;
+const IconLeftMain = styled(IconRightMain)``;
 
 const iconStyle = {
 	height: "100%",
