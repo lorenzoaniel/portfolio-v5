4b7eb11bb9fa2bf3530ad4bbe9039020dcf55da6
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-06 14:41:43 -0700

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { navMenuToggleSlice } from "./features/navBarToggleSlice";
+import { pagesInfoSlice } from "./features/pagesInfoSlice";
 
 export const store = configureStore({
 	reducer: {
 		navMenuToggleSlice: navMenuToggleSlice.reducer,
+		pagesInfoSlice: pagesInfoSlice.reducer,
 	},
 });
 
