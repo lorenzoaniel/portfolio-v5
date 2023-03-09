6ef17753dbcb05112020019ea57a03fe666febc3
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-08 20:36:46 -0700

    Update Contact.tsx

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+import DefaultForm from "../../components/Form/DefaultForm";
+import FloatingCrystalHeading from "../../components/Heading/FloatingCrystalHeading";
+
 const Contact: React.FC = () => {
-	return <Main id={"contact"}></Main>;
+	return (
+		<Main id={"contact"}>
+			<FloatingCrystalHeading title={"Contact Me"} />
+			<DefaultForm />
+		</Main>
+	);
 };
 
 const Main = styled(motion.section)`
-	height: 100rem;
+	border: 0.1rem solid blue;
+
+	height: fit-content;
 	background: transparent;
 	/* background: var(--palette-color-light); */
+	z-index: 1;
+	display: flex;
+	flex-direction: column;
 `;
 
 export default Contact;
