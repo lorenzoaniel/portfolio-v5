cac67c3f300a667f68f9651673251c9fff179283
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-09 21:01:17 -0700

    Update DefaultFooter.tsx

--- src/components/Footer/DefaultFooter.tsx
+++ src/components/Footer/DefaultFooter.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 const DefaultFooter = () => {
-	return <div>DefaultFooter</div>;
+	return (
+		<Main>
+			<small>&copy; Copyright 2023, Mikhail Lorenzo Aniel</small>
+		</Main>
+	);
 };
+
+const Main = styled.footer`
+	width: 100%;
+	height: 10rem;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	box-shadow: 0 0 0.5rem 0.2rem var(--palette-color-medium);
+	backdrop-filter: blur(0.1rem);
+`;
 
 export default DefaultFooter;
